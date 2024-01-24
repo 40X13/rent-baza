0e8a6dc71d8462600e23c2361e790a770c84c5d2
Pavel Arlou <arloupav@gmail.com>
2024-01-24 21:29:29 +0300

    init: start rent-baza projekt

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Sidebar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
